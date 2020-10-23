9d64c03375c24d3b575e626cff753959b24bcc3c
emaapirata <emaapirata@gmail.com>
2020-10-23 16:31:31 -0300

    first commit

---
+++ assets/js/juego.js
@@ -0,0 +1,151 @@
+const miModulo = (() => {
+  "use strict";
+
+  let deck = [];
+  const tipos = ["C", "D", "H", "S"],
+    especiales = ["A", "J", "Q", "K"];
+
+  let puntosJugadores = [];
+  // Referencias del HTML
+  const btnPedir = document.querySelector("#btnPedir"),
+    btnDetener = document.querySelector("#btnDetener"),
+    btnNuevo = document.querySelector("#btnNuevo");
+
+  const divCartas = document.querySelectorAll(".divCartas"),
+    puntosHTML = document.querySelectorAll("small");
+
+  const inicializarJuego = (numJugadores = 1) => {
+    deck = crearDeck();
+    puntosJugadores = [];
+    for (let i = 0; i <= numJugadores; i++) {
+      puntosJugadores.push(0);
+    }
+  };
+  // Esta función crea un nuevo deck
+  const crearDeck = () => {
+    deck = [];
+    for (let i = 2; i <= 10; i++) {
+      for (let tipo of tipos) {
+        deck.push(i + tipo);
+      }
+    }
+
+    for (let tipo of tipos) {
+      for (let esp of especiales) {
+        deck.push(esp + tipo);
+      }
+    }
+    return _.shuffle(deck);
+  };
+
+  // Esta función me permite tomar una carta
+  const pedirCarta = () => {
+    if (deck.length === 0) {
+      throw "No hay cartas en el deck";
+    }
+    return deck.pop();
+  };
+
+  // pedirCarta();
+  const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+  };
+
+  //Turno: 0 jugador 1 y el ultimo la pc
+  const acumularPuntos = (turno, carta) => {
+    puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta);
+    puntosHTML[turno].innerText = puntosJugadores[turno];
+    return puntosJugadores[turno];
+  };
+
+  const agregarCartaHTML = (carta, turno) => {
+    // <img class="carta" src="assets/cartas/2C.png">
+    const imgCarta = document.createElement("img");
+    imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
+    imgCarta.classList.add("carta");
+    divCartas[turno].append(imgCarta);
+  };
+
+  // turno de la computadora
+
+  const determinarGanador = () => {
+
+    const [puntosMinimos, puntosComputadora] = puntosJugadores;
+
+    setTimeout(() => {
+      if (puntosComputadora === puntosMinimos) {
+        alert("Nadie gana :(");
+      } else if (puntosMinimos > 21) {
+        alert("Computadora gana");
+      } else if (puntosComputadora > 21) {
+        alert("Jugador Gana");
+      } else {
+        alert("Computadora Gana");
+      }
+    }, 100);
+
+  }
+
+  const turnoComputadora = (puntosMinimos) => {
+    let puntosComputadora = 0;
+    do {
+      const carta = pedirCarta();
+
+      puntosComputadora = acumularPuntos(puntosJugadores.length - 1, carta);
+      agregarCartaHTML(carta, divCartas.length - 1);
+
+      if (puntosMinimos > 21) {
+        break;
+      }
+    } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+    determinarGanador();
+
+  };
+
+  // Eventos
+  btnPedir.addEventListener("click", () => {
+    const carta = pedirCarta();
+
+    const puntosJugador = acumularPuntos(0, carta);
+
+    agregarCartaHTML(carta, 0);
+
+    if (puntosJugador > 21) {
+      console.warn("Lo siento mucho, perdiste");
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoComputadora(puntosJugador);
+    } else if (puntosJugador === 21) {
+      console.warn("21, genial!");
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoComputadora(puntosJugador);
+    }
+  });
+
+  btnDetener.addEventListener("click", () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    turnoComputadora(puntosJugadores[0]);
+  });
+
+  btnNuevo.addEventListener("click", () => {
+    console.clear();
+    inicializarJuego(1);
+
+    for (const puntos of puntosHTML) {
+      puntos.innerHTML = "";
+    }
+    for (const divC of divCartas) {
+      divC.innerHTML = "";
+    }
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+  });
+
+  return 'Hola Mundo'
+})();
